3412ee3dc610c00894c9452025e0adc2bc34ee12
callmePjotr <91414106+callmePjotr@users.noreply.github.com>
2022-08-09 16:31:16 +0200

    Create myXSSStrip.java

---
+++ myXSSStrip.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class myXSSStrip {
+
+    protected static String stripXSS(String value) {
+        String cleanValue = null;
+        if (value != null) {
+            cleanValue = Normalizer.normalize(value, Normalizer.Form.NFD);
+
+            // Avoid null characters
+            cleanValue = cleanValue.replaceAll("\0", "");
+
+            // Avoid anything between script tags
+            Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid anything in a src='...' type of expression
+            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Remove any lonesome </script> tag
+            scriptPattern = Pattern.compile("</script>", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Remove any lonesome <script ...> tag
+            scriptPattern = Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid eval(...) expressions
+            scriptPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid expression(...) expressions
+            scriptPattern = Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid javascript:... expressions
+            scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid vbscript:... expressions
+            scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+
+            // Avoid onload= expressions
+            scriptPattern = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            cleanValue = scriptPattern.matcher(cleanValue).replaceAll("");
+        }
+        return cleanValue;
+    }
+}
